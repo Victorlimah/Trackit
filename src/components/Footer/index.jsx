77cad3f6fe395b1c30472d34ff6cf7811d9598e2
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-04-02 01:39:25 -0300

    create footer routes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,29 @@
 import * as S from "./style";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer({ image }) {
+  const navigate = useNavigate();
   return (
     <>
       <S.Footer>
-        <h3>Hábitos</h3>
+        <h3 onClick={() => habits()}>Hábitos</h3>
         <S.Today>
-          <h3>Hoje</h3>
+          <h3 onClick={() => today()}>Hoje</h3>
         </S.Today>
-        <h3>Histórico</h3>
+        <h3 onClick={() => history()}>Histórico</h3>
       </S.Footer>
     </>
   );
+
+  function habits() {
+    navigate("/habits");
+  }
+
+  function today() {
+    navigate("/today");
+  }
+
+  function history() {
+    navigate("/history");
+  }
 }
